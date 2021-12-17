7d33631e3e4935afc2302eceb483adf59dd0f130
Vinicius Reis <vinicius.reis@msn.com>
2021-12-17 17:49:04 -0300

    mapeia agregado de tarefa

---
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -0,0 +1,34 @@
+package dev.wakandaacademy.produdoro.tarefa.domain;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Tarefa")
+public class Tarefa {
+	@Id
+	private UUID idTarefa;
+	private String nome;
+	private String descricao;
+	@Indexed
+	private UUID idUsuario;
+	@Indexed
+	private UUID idArea;
+	@Indexed
+	private UUID idProjeto;
+	@Builder.Default
+	private StatusTarefa status = StatusTarefa.A_FAZER;
+	
+}
