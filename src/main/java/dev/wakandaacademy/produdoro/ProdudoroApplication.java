4ede488f02f7c366b4b70c7406d3dbfb3ebd88ca
Vinicius Reis <vinicius.reis@msn.com>
2021-12-17 09:19:28 -0300

    init commit

---
+++ src/main/java/dev/wakandaacademy/produdoro/ProdudoroApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.produdoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdudoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdudoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/produdoro/ProdudoroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.produdoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdudoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
