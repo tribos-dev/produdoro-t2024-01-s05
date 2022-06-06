3d5fe0f84ad092c57b6a53a4046957a7b2e4fa7d
Vinicius Reis <vinicius.reis@msn.com>
2022-06-06 19:03:21 -0300

    implementa credencial para security

--- src/main/java/dev/wakandaacademy/produdoro/credencial/domain/Credencial.java
+++ src/main/java/dev/wakandaacademy/produdoro/credencial/domain/Credencial.java
@@ -1,30 +1,84 @@
 package dev.wakandaacademy.produdoro.credencial.domain;
 
+import java.util.Collection;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Document(collection = "Credencial")
-public class Credencial {
+public class Credencial implements UserDetails {
 	@MongoId(targetType = FieldType.STRING)
 	@Getter
 	private String usuario;
+	
+	@NotNull
+	@Size(max = 60)
 	private String senha;
+	
+	@Getter
+	private boolean validado;
 
-	public Credencial(String usuario, String senha) {
+	public Credencial(String usuario, @NotNull String senha) {
 		this.usuario = usuario;
-		this.senha = new BCryptPasswordEncoder().encode(senha);
+		this.senha = senha;
+		this.validado = true;
+	}
+
+	public void encriptaSenha() {
+		var encriptador = new BCryptPasswordEncoder();
+		this.senha = encriptador.encode(this.senha);
 	}
 	
-	public String getPassword() {
-		return senha;
+	public void validaCredencial() {
+		this.validado = true;
 	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.usuario;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	private static final long serialVersionUID = 1L;
 }
