fb14ed9a0b40e5fbe43e008ddf3191d7e106e77a
Vinicius Reis <vinicius.reis@msn.com>
2022-06-06 19:26:41 -0300

    implementa app service de credencial

--- src/main/java/dev/wakandaacademy/produdoro/credencial/application/service/CrendencialService.java
+++ src/main/java/dev/wakandaacademy/produdoro/credencial/application/service/CrendencialService.java
@@ -4,14 +4,24 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
 
+import dev.wakandaacademy.produdoro.credencial.application.repository.CredencialRepository;
+import dev.wakandaacademy.produdoro.credencial.domain.Credencial;
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class CrendencialService implements CredencialApplicationService {
-
+	private final CredencialRepository credencialRepository;
+	
 	@Override
 	public void criaNovaCredencial(@Valid UsuarioNovoRequest usuarioNovo) {
-		//TODO Implementar metodo para criar nova creencial
+		log.info("[start] CrendencialService - criaNovaCredencial");
+		var novaCredencial = new Credencial(usuarioNovo.getSenha(), usuarioNovo.getSenha());
+		credencialRepository.salva(novaCredencial);
+		log.info("[finish] CrendencialService - criaNovaCredencial");
 	}
 
 }
