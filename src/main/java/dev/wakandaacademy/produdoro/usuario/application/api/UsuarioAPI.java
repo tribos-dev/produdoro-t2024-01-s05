14bd4f07accd3b30856f7637d3aec7daf9b74621
Vinicius Reis <vinicius.reis@msn.com>
2022-06-14 17:45:40 -0300

    modifica post usuario para public

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/v1/usuario")
+@RequestMapping(value = "/v1/public/usuario")
 public interface UsuarioAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
