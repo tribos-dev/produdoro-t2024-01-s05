ca37ead03b613fd20fe6eb69c751beb255925b65
Maabia <maabia.ferry16@gmail.com>
2022-09-12 22:46:54 -0300

    alterações na classe Tarefa e criação de enum StatusAtivacaoTarefa

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -2,6 +2,7 @@ package dev.wakandaacademy.produdoro.tarefa.domain;
 
 import java.util.UUID;
 
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,14 +13,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Document(collection = "Tarefa")
 public class Tarefa {
+
 	@Id
 	private UUID idTarefa;
+	@NotBlank
 	private String descricao;
 	@Indexed
 	private UUID idUsuario;
@@ -27,7 +32,19 @@ public class Tarefa {
 	private UUID idArea;
 	@Indexed
 	private UUID idProjeto;
-	@Builder.Default
 	private StatusTarefa status = StatusTarefa.A_FAZER;
+	private StatusAtivacaoTarefa statusAtivacao;
+	private int contagemPomodoro;
+
+	public Tarefa(TarefaRequest tarefaRequest) {
+		this.idTarefa = UUID.randomUUID();
+		this.idUsuario = tarefaRequest.getIdUsuario();
+		this.descricao = tarefaRequest.getDescricao();
+		this.idArea = tarefaRequest.getIdArea();
+		this.idProjeto = tarefaRequest.getIdProjeto();
+		this.status = StatusTarefa.A_FAZER;
+		this.statusAtivacao = StatusAtivacaoTarefa.INATIVA;
+		this.contagemPomodoro = 1;
+	}
 	
 }
