d0344c9d61e6e44c1448ad2af16609cacc44870b
Maabia <maabia.ferry16@gmail.com>
2022-10-08 19:40:38 -0300

    criado método GET em Tarefa API e classe TarefaListResponse

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -4,12 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/tarefa")
 public interface TarefaAPI {
-
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     TarefaIdResponse postNovaTarefa(@RequestBody @Valid TarefaRequest tarefaRequest);
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    List<TarefaListResponse> listaTarefasPorIdUsuario(@RequestParam("idUsuario")Optional<String> idUsuario);
 }
