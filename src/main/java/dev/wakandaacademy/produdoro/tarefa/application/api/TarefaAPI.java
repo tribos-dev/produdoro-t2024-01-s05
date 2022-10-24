46a178a0e518163d1d1e48d6af631af80fb1f4bb
Vinicius Reis <vinicius.reis@msn.com>
2022-10-24 20:55:57 -0300

    pega token auth em get tarefa detalhada por id

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -1,12 +1,18 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/tarefa")
@@ -17,6 +23,7 @@ public interface TarefaAPI {
 
     @GetMapping("/{idTarefa}")
     @ResponseStatus(code = HttpStatus.OK)
-    TarefaDetalhadoResponse detalhaTarefa(@PathVariable UUID idTarefa);
+    TarefaDetalhadoResponse detalhaTarefa(@RequestHeader(name = "Authorization",required = true) String token, 
+    		@PathVariable UUID idTarefa);
 
 }
--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -1,34 +1,35 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RestController;
+
 import dev.wakandaacademy.produdoro.tarefa.application.service.TarefaService;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @Log4j2
 @RequiredArgsConstructor
 public class TarefaRestController implements TarefaAPI {
 
-    private final TarefaService tarefaService;
+	private final TarefaService tarefaService;
 
-    public TarefaIdResponse postNovaTarefa(TarefaRequest tarefaRequest) {
-        log.info("[inicia]  TarefaRestController - postNovaTarefa  ");
-        TarefaIdResponse tarefaCriada = tarefaService.criaNovaTarefa(tarefaRequest);
-        log.info("[finaliza]  TarefaRestController - postNovaTarefa");
-        return tarefaCriada;
-    }
-    @Override
-    public TarefaDetalhadoResponse detalhaTarefa(UUID idTarefa) {
-        log.info("[inicia] TarefaRestController - detalhaTarefa");
-        Tarefa tarefa = tarefaService.detalhaTarefa(idTarefa);
-        log.info("[finaliza] TarefaRestController - detalhaTarefa");
-        return new TarefaDetalhadoResponse(tarefa);
-    }
+	public TarefaIdResponse postNovaTarefa(TarefaRequest tarefaRequest) {
+		log.info("[inicia]  TarefaRestController - postNovaTarefa  ");
+		TarefaIdResponse tarefaCriada = tarefaService.criaNovaTarefa(tarefaRequest);
+		log.info("[finaliza]  TarefaRestController - postNovaTarefa");
+		return tarefaCriada;
+	}
+
+	@Override
+	public TarefaDetalhadoResponse detalhaTarefa(String token, UUID idTarefa) {
+		log.info("[inicia] TarefaRestController - detalhaTarefa");
+		log.info("[token] {}", token);
+		Tarefa tarefa = tarefaService.detalhaTarefa(idTarefa);
+		log.info("[finaliza] TarefaRestController - detalhaTarefa");
+		return new TarefaDetalhadoResponse(tarefa);
+	}
 
 }
