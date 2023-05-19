ea1ea0efc141d588b977e86ce6732558a9da2999
Felipendev <felipe.psic@outlook.com.br>
2023-05-19 11:07:26 -0300

    Implementa classe de teste de tarefa

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,5 +1,53 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
+import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+
+@ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
 
+    //	@Autowired
+    @InjectMocks
+    TarefaApplicationService tarefaApplicationService;
+
+    //	@MockBean
+    @Mock
+    TarefaRepository tarefaRepository;
+
+    @Test
+    void deveRetornarIdTarefaNovaCriada() {
+        TarefaRequest request = getTarefaRequest();
+        when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
+
+        TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
+
+        assertNotNull(response);
+        assertEquals(TarefaIdResponse.class, response.getClass());
+        assertEquals(UUID.class, response.getIdTarefa().getClass());
+    }
+
+
+
+    public TarefaRequest getTarefaRequest() {
+        TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
+        return request;
+    }
 }
