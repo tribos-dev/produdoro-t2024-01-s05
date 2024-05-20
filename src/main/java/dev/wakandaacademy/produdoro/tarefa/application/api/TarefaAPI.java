752766095351b2d09a6bfcb2285f0603ee2284c4
Pedroh <pedrooohsss@gmail.com>
2024-05-20 19:11:55 -0300

    implementa camada controller

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -1,5 +1,6 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import javax.validation.Valid;
@@ -25,5 +26,9 @@ public interface TarefaAPI {
     @ResponseStatus(code = HttpStatus.OK)
     TarefaDetalhadoResponse detalhaTarefa(@RequestHeader(name = "Authorization",required = true) String token, 
     		@PathVariable UUID idTarefa);
+    
+    @GetMapping("/buscaTarefasDoUsuario/{idUsuario}")
+    List<TarefaListReponse> buscaTarefasPorUsuario(@RequestHeader(name = "Authorization",required = true) String token, 
+    		@PathVariable UUID idUsuario); 
 
 }
--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -1,5 +1,6 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -40,6 +41,13 @@ public class TarefaRestController implements TarefaAPI {
 		String usuario = tokenService.getUsuarioByBearerToken(token).orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
 		log.info("[usuario] {}", usuario);
 		return usuario;
+	}
+
+	@Override
+	public List<TarefaListReponse> buscaTarefasPorUsuario(String token, UUID idUsuario) {
+		log.info("[inicia] TarefaRestController - buscaTarefasPorUsuario");
+		log.info("[finaliza] TarefaRestController - buscaTarefasPorUsuario");
+		return null;
 	}
 
 }
