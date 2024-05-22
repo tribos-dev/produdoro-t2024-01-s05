7d71b6eef282b3d3f5cb8560a6ac2de24ce13c7e
Mateus Pereira <mateusp.s@outlook.com>
2024-05-21 21:33:14 -0300

    implementa camada api

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
@@ -2,6 +2,9 @@ package dev.wakandaacademy.produdoro.usuario.application.api;
 
 import javax.validation.Valid;
 
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
 	private final UsuarioService usuarioAppplicationService;
+	private final TokenService tokenService;
 
 	@Override
 	public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
@@ -32,5 +36,13 @@ public class UsuarioController implements UsuarioAPI {
 		UsuarioCriadoResponse buscaUsuario = usuarioAppplicationService.buscaUsuarioPorId(idUsuario);
 		log.info("[finaliza] UsuarioController - buscaUsuarioPorId");
 		return buscaUsuario;
+	}
+
+	@Override
+	public void mudaParaPausaLonga(String token, UUID idUsuario) {
+		log.info("[inicia] - UsuarioController - mudaParaPausaLonga");
+		String usuario = tokenService.getUsuarioByBearerToken(token).orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
+		usuarioAppplicationService.mudaParaPausaLonga(usuario, idUsuario);
+		log.info("[finaliza] - UsuarioController - mudaParaPausaLonga");
 	}
 }
