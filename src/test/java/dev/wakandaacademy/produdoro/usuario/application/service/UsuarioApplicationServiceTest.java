89f2429d9dcf1064e481c1c29f05986c5db6ae0d
kevinnjr9 <kevinnjr@outlook.com>
2024-05-23 19:29:43 -0300

    implementa testes unitarios

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,41 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioApplicationServiceTest {
 
+    @InjectMocks
+    UsuarioApplicationService usuarioApplicationService;
+
+    @Mock
+    UsuarioRepository usuarioRepository;
+
+    @Test
+    void deveMudarStatusParaFoco(){
+        //Dado
+        Usuario usuario = DataHelper.createUsuario();
+        //Quando
+        when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        usuarioApplicationService.mudaStatusParaFoco(usuario.getEmail(),usuario.getIdUsuario());
+        //Entao
+        assertEquals(StatusUsuario.FOCO,usuario.getStatus());
+        verify(usuarioRepository,times(1)).buscaUsuarioPorId(usuario.getIdUsuario());
+        verify(usuarioRepository,times(1)).buscaUsuarioPorEmail(usuario.getEmail());
+        verify(usuarioRepository,times(1)).salva(usuario);
+    }
 }
